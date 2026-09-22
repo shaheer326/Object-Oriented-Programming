f156b858d93cd45785254a8e8977977e1c40e431
shaheer326 <shaheerkhan1211@gmail.com>
2026-09-22 17:56:27 +0500

    2

---
+++ activity2.py
@@ -0,0 +1,10 @@
+class Vehical:
+    def __init__(self, max_speed, milaege):
+
+        self.max_speed = max_speed
+        self.milaege = milaege
+
+modelX = Vehical(240, 18)
+
+print("Model max speed: ", modelX.max_speed)
+print("Model milaege: ", modelX.milaege)
